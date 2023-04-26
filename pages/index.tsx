22b40ebffaf305d8186f4527db7d9130eafb8d97
Taeyooooon <eric568@naver.com>
2023-04-26 19:50:09 +0900

    Delete: home page Head

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,8 @@
 import React from 'react'
-import Head from 'next/head'
 
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>DevMe</title>
-      </Head>
       <div>메인페이지</div>
     </div>
   )
